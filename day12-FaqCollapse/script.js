d2b15ae34a2b8a46e2254358f6d2c17dcb5ae8cf
dev1d123 <dhuamanio@unsa.edu.pe>
2024-08-01 08:40:27 -0500

    Day 12, complete

--- day12-FaqCollapse/script.js
+++ day12-FaqCollapse/script.js
@@ -1,2 +1,13 @@
 //seleccionar todos los botones
 //a cada boton darle un addeventlistener para darle la clase active.
+const buttons = document.querySelectorAll('.faq-toggle');
+console.log(buttons);
+
+buttons.forEach(button => {
+    button.addEventListener('click', () =>{
+        button.parentElement.classList.toggle('active');
+        /*
+        Si la clase está presente en el elemento, toggle() la elimina; si no está presente, toggle() la añade.
+        */
+    })
+})
