0b0e13befc855b19faa26d598e6824a8d98cc404
dev1d123 <dhuamanio@unsa.edu.pe>
2024-08-11 21:43:48 -0500

    Day30, complete

---
+++ day30-AutoTextEffect/script.js
@@ -0,0 +1,24 @@
+const textElement = document.getElementById('text')
+const speedElement = document.getElementById('speed')
+
+const text = 'We Love Programming!'
+
+let idx = 1
+let speed = 300 / speedElement.value
+
+
+writeText()
+
+function writeText(){
+    textElement.innerHTML = text.slice(0, idx)
+
+    idx++;
+
+    if(idx > text.length){
+        idx = 1;
+    }
+    setTimeout(writeText, speed)
+
+}
+
+speedElement.addEventListener('input', (e) => speed = 300 / e.target.value)
